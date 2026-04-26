d83cefa93eede968cde49660ed10e12d79317c99
Igor Kotlik <164913642+ik-wsb@users.noreply.github.com>
2026-04-26 13:13:03 +0200

    Update CORS middleware to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -98,7 +98,7 @@ async def get_air_quality(city: str, db: Session = Depends(get_db)):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], # Ewentualnie ["*"] do testów
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
